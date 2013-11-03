3231ac3e6551d67fa3b8fcc6aaea435305fd1932
yanbab <yanbab@gmail.com>
2013-11-03 21:07:06 +0100

    fixed featured apps

--- data/featured.js
+++ data/featured.js
@@ -1,1 +1,28 @@
-var featured = ["accountsservice","acl","acpi"];
+var featured = [
+    /* Graphics */
+    "gimp",
+    "inkscape",
+    "scribus",
+    "agave",
+    "blender",
+    /* Tools */
+    "gedit",
+    "gnome-terminal",
+    /* Internet */
+    "firefox",
+    "thunderbird",
+    "evolution",
+    "empathy",
+    "pidgin",
+    "flashplugin-nonfree",
+    /*Audio Video*/
+    "audacity",
+    "pitivi",
+    "openshot",
+    "rhythmbox",
+    
+    "libreoffice",
+    "abiword",
+    "gnumeric",
+    "gnucash"
+];
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ apt:Finder
 
 An experiment to create an easy to use and fast application manager for Ubuntu and Debian.
 
-Web Site : http://www.yanbab.net/projects/aptfinder/site
+*Web Site* : http://www.yanbab.net/projects/aptfinder/site
 
 Demo : http://www.yanbab.net/projects/aptfinder/html
 
